45326cc118c26e6155c9265990c43e43078a17b6
Jared Hanson <jaredhanson@gmail.com>
2012-04-26 21:45:45 -0700

    Initial implementation and tests.

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 define(function() {
   
+  var generator;
+  
   function xml(name, attrs) {
+    if (!generator) { init(); }
+    
+    var doc = generator.createElement(name);
+    return doc;
+  }
+  
+  xml.stringify = function(node) {
+    var s = new XMLSerializer();
+    return s.serializeToString(node);
+  }
+  
+  
+  function init() {
+    if (window.ActiveXObject) {
+      generator = new ActiveXObject("Microsoft.XMLDOM");
+      generator.appendChild(doc.createElement('document'));
+    } else {
+      generator = document.implementation.createDocument('http://xml.anchorjs.org', 'document', null);
+    }
   }
   
   return xml;
--- tests/test.xml.js
+++ tests/test.xml.js
@@ -10,6 +10,23 @@ function(xml, chai) {
       expect(xml).to.be.a('function');
     });
     
+    it('should have a stringify function', function() {
+      expect(xml.stringify).to.be.a('function');
+    });
+    
+    describe('create with name', function() {
+      var doc = xml('html');
+      
+      it('should be an instance of Element', function() {
+        expect(doc).to.be.an.instanceOf(Node);
+        expect(doc).to.be.an.instanceOf(Element);
+      });
+      it('should serialize to string', function() {
+        var str = xml.stringify(doc);
+        expect(str).to.be.equal('<html/>');
+      });
+    });
+    
   });
   
   return { name: "test.xml" }
