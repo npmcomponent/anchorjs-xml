f364287e74a057c75ca03c9751340e06c77db758
Jared Hanson <jaredhanson@gmail.com>
2012-04-28 11:36:53 -0700

    Document xml and stringify.

--- main.js
+++ main.js
@@ -2,10 +2,30 @@ define(['./lib/document',
         './lib/utils'],
 function(Document, utils) {
   
+  /**
+   * Returns an XML document with usable building and traversing functions.
+   *
+   * Examples:
+   *
+   *     xml('body');
+   *
+   *     xml('message', { to: 'romeo', from: 'juliet' });
+   *
+   *     xml('http://www.w3.org/1999/xhtml', 'div', { id: 'foo' });
+   *
+   * @param {String} ns
+   * @param {String} name
+   * @param {Object} attrs
+   * @return {Document} XML document wrapper
+   * @api public
+   */
   function xml(ns, name, attrs) {
     return new Document(ns, name, attrs);
   }
   
+  /**
+   * Expose utilities.
+   */
   xml.stringify = utils.stringify;
   
   return xml;
