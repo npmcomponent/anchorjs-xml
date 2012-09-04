2f4333f06e921f835f78caa3b9d50244a648b4cc
Jared Hanson <jaredhanson@gmail.com>
2012-09-04 14:10:04 -0700

    Use volo-managed dependencies in tests.

--- tests/runner.js
+++ tests/runner.js
@@ -1,16 +1,14 @@
 require.config({
   paths:{
-    'anchor': '../../anchor',
     'xml': '../',
-    'mocha': 'vendor/mocha/1.0.1/mocha',
-    'chai': 'vendor/chai/0.5.2/chai'
+    'mocha': 'vendor/mocha/mocha',
+    'chai': 'vendor/chai/chai'
   }
 });
 
 require(['require',
          'mocha',
          'chai'],
-
 function(require, _mocha, _chai) {
   mocha.setup('bdd');
   
